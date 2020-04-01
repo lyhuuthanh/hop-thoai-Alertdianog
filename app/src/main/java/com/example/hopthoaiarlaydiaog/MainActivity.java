eb6c1c7e6f2bec9765e8e10acacec8551dbcd30e
lyhuuthanh <lyhuuthanhtv@gmail.com>
2020-04-01 17:40:57 +0700

    hopthoai2

--- app/src/main/java/com/example/hopthoaiarlaydiaog/MainActivity.java
+++ app/src/main/java/com/example/hopthoaiarlaydiaog/MainActivity.java
@@ -3,9 +3,11 @@
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
  public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +27,32 @@ import android.widget.Button;
                 builder.setMessage("ban co chac chan muon vay khong?");
                 builder.setIcon(R.mipmap.ic_launcher);
 
+                builder.setPositiveButton("CO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"Cam on ban",Toast.LENGTH_LONG).show();
+
+                    }
+                });
+                builder.setNegativeButton("khong", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "ban khong dong y", Toast.LENGTH_LONG).show();
+                    }
+                });
+                        builder.setNegativeButton("Huy", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                     Toast.makeText(MainActivity.this,"ban thoat ra",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        builder.setNeutralButton("Thoat", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this,"ban da thoat",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        builder.show();
             }
         });
 
